9b074a109a49510e0709309388225a0cdd3dd863
Kanester <pampagkieven091@gmail.com>
2025-02-07 18:47:23 +0800

    deploy

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,59 +1,55 @@
-import * as esbuild from "esbuild";
-import { copy } from "esbuild-plugin-copy";
-import * as minifier from "html-minifier-terser";
-import fs from "fs/promises";
-import fg from "fast-glob";
-import path from "path";
+import * as esbuild from 'esbuild';
+import { copy } from 'esbuild-plugin-copy';
+import { sassPlugin } from 'esbuild-sass-plugin';
+import * as minifier from 'html-minifier-terser';
+import fs from 'fs/promises';
+import path from 'path';
+import fg from 'fast-glob';
 
-const outputDir = "./dist";
-
+//minifying html
 await Promise.all(
-    (await fg("src/**/*.html")).map(async file => {
-        const outFile = path.join(outputDir, path.relative("src", file));
-        await fs.mkdir(path.dirname(outFile), { recursive: true });
-        await fs.writeFile(
-            outFile,
-            await minifier.minify(await fs.readFile(file, "utf8"), {
-                collapseWhitespace: true,
-                removeComments: true,
-                minifyCSS: true,
-                minifyJS: true
-            })
-        );
+  (await fg('src/**/*.html')).map(async file => {
+    await fs.mkdir(
+      path.dirname(path.join('./dist', path.relative('src', file))),
+      { recursive: true }
+    );
+    await fs.writeFile(
+      path.join('./dist', path.relative('src', file)),
+      await minifier.minify(await fs.readFile(file, 'utf8'), {
+        collapseWhitespace: true,
+        removeComments: true,
+        minifyCSS: true,
+        minifyJS: true
+      })
+    );
+  })
+);
+
+//minifying css and js
+const entrypoints = await fg([
+  'src/**/*.js',
+  'src/**/*.ts',
+  'src/**/*.css',
+  'src/**/*.scss'
+]);
+
+esbuild.build({
+  entryPoints: entrypoints,
+  bundle: true,
+  outdir: './dist',
+  minify: true,
+  plugins: [
+    sassPlugin(),
+    copy({
+      assets: {
+        from: (await fg('src/**/*')).filter(
+          f =>
+            !['.html', '.js', '.ts', '.css', '.scss'].some(ext =>
+              f.endsWith(ext)
+            )
+        ),
+        to: './dist'
+      }
     })
-);
-
-const ctx = await esbuild.context({
-    entryPoints: ["src/main.js"],
-    bundle: true,
-    outdir: outputDir,
-    minify: true,
-    plugins: [
-        copy({
-            assets: {
-                from: (await fg("src/**/*")).filter(
-                    f => !f.endsWith(".html")
-                ).filter(f => !f.endsWith(".js")),
-                to: ["./"]
-            },
-            watch: true
-        })
-    ]
+  ]
 });
-
-process.argv.includes("--serve")
-    ? ctx
-          .serve({ servedir: outputDir })
-          .then(({ host, port }) =>
-              console.log(`🚀 Serving at http://${host || "localhost"}:${port}`)
-          )
-    : (await ctx.rebuild(),
-      console.log("✅ Build complete!"),
-      await ctx.dispose());
-
-process.on(
-    "SIGINT",
-    async () => (
-        await ctx.dispose(), console.log("\n🧹 Cleanup done!"), process.exit(0)
-    )
-);
